e5a0ccffc9beff7d7500f6121a28e377645a4ecc
Filip Cák <71824428+filipcak@users.noreply.github.com>
2023-11-22 16:52:36 +0100

    Zadanie 4 - pripraveny template

---
+++ Pages/Validation.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SIPVS_NT.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ValidationModel : PageModel
+{
+   // todo: add your code here
+}
+
+
